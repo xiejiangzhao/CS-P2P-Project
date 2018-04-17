615722aad92e25d62b21314859e9fd1c5508b027
谢江钊 <10751583@qq.com>
2018-04-17 19:26:38 +0800

    v0.01
    试图外网连接

---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+print('Server runs at '+HOST+' '+str(PORT))
+print('Wait for Connection...')
+ 
+while True:
+    conn, addr = s.accept()
+    print('Connected by '+addr[0])
+
+    while True:
+        msg_send=input("Input msg to send")
+        conn.send(bytes(msg_send,'utf8'))
+        print("Waiting to msg")
+        data = conn.recv(1024)
+        print ("Msg received")
+        print(str(data,'utf8'))
+        
+        
+
+# conn.close()
